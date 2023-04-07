8a33ca4cf4a1738ce92a488096bb1aa0bbd19113
limengkai <li_mengkai@binc.jp>
2023-04-07 17:26:21 +0900

    dockerize

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # simple-ec-server
+本地启动
+```
+docker-compose --env-file env.dev up -d
+```
--- simpleEcServer/settings.py
+++ simpleEcServer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'simpleEcServer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'simple_ec_db',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'aa11',
-        'PORT': '33060',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
