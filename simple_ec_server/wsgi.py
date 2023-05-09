279685dc662e0e18105e31ed7f3b23e603bd0748
kk <iamibe.kai@gmail.com>
2023-05-09 21:04:44 +0900

    rename project

--- simple_ec_server/wsgi.py
+++ simple_ec_server/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for simpleEcServer project.
+WSGI config for simple_ec_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpleEcServer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_ec_server.settings')
 
 application = get_wsgi_application()
